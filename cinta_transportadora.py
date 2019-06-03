3282370f186eade30b2c5aceb53e490be425a469
sergio-jmnzanton <51339054+sergio-jmnzanton@users.noreply.github.com>
2019-06-03 22:38:37 +0200

    Update cinta_transportadora.py

--- cinta_transportadora.py
+++ cinta_transportadora.py
@@ -1,38 +1,40 @@
-import time
-import speech_recognition as sr
+import time 
+import speech_recognition as sr #PAQUETE PARA EL RECONOCIMIENTO DE VOZ
 import RPi.GPIO as GPIO
-import mysql.connector
-import datetime
-import requests
+import mysql.connector #PARA EL USO DE MySQL EN Python
+import datetime #PARA DETENER LA CINTA TRANSCURRIDOS 10 SEGUNDOS
+import requests #PARA EL USO DE IFTTT (Webhooks + SMS)
 
 #CONEXION CON MYSQL
-
 mydb = mysql.connector.connect(
 	user='admin',
 	password='root',
 	host='127.0.0.1',
-    database='cinta'
+    	database='cinta'
 )
 
 mycursor = mydb.cursor()
 
+
+#CONTROLES DEL PAQUETE SpeechRecognition
 r = sr.Recognizer()
 r.pause_threshold = 0.5
 m = sr.Microphone()
 r.operation_timeout = 1
 
+#SETUP DEL GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT) #PIN del buzzer
 GPIO.setup(16, GPIO.IN) #PIN del sensor IR
 
-texto = 0
+texto = 0 #VARIABLE DONDE SE GUARDA EN FORMATO STRING LA ORDEN DE VOZ INTRODUCIDA
 pin_ldr= 7 #PIN del fotoresistor
 
 
 with m as source:
     r.adjust_for_ambient_noise(source)  #Ajustar el ruido ambiente al microfono (solo se realiza una vez al principio)
 
-def callback(recognizer, audio): #Funcion ejecutada en un hilo secundario (background thread) que escucha activamente el microfono
+def callback(recognizer, audio): #FUNCION EJECUTADA EN UN HILO SECUNDARIO (background thread) QUE ESCUCHA ACTIVAMENTE EL MICROFONO
     
     try:
         global texto
@@ -41,10 +43,10 @@ def callback(recognizer, audio): #Funcion ejecutada en un hilo secundario (backg
     except sr.UnknownValueError:
         print("No se reconocio la orden")
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("Could not request results from Google Speech Recognition service; {0}".format(e)) #EXCEPCION POR DEFECTO DE SpeechRecognition EN CASO DE NO HABER CONEXION A INTERNET
 
-def rc_time(): #Obtener lectura analogica del fotoresistor.
-
+	def rc_time(): #OBTENCION DE LECTURA ANALOGICA DEL FOTORRESISTOR LDR
+    
     count = 0
 
     GPIO.setup(7, GPIO.OUT)
@@ -58,19 +60,19 @@ def rc_time(): #Obtener lectura analogica del fotoresistor.
 
     return count
 
-def iniciar_cinta(): #Funcion que pone en marcha la cinta
+def iniciar_cinta(): #FUNCION QUE PONE EN MARCHA LA CINTA
     
-    requests.post("https://maker.ifttt.com/trigger/cinta_iniciada/with/key/bdXiQ8Ftr3DpQ0Igri61H_") #Implementación de IFTTT
+    requests.post("https://maker.ifttt.com/trigger/cinta_iniciada/with/key/bdXiQ8Ftr3DpQ0Igri61H_") #IMPLEMENTACION DE IFTTT (MANDA UN SMS INFORMATIVO A UN MOVIL CUANDO SE INICIA LA CINTA)
     
-    inicial = int(round(time.time()*1))
+    inicial = int(round(time.time()*1)) #SEGUNDOS
     diferencia = 0
     numPieza = 0
     
-    GPIO.setup(15, GPIO.OUT) #Activar el motor
-    p = GPIO.PWM(15, 50) #Enviar 50 pulsos al motor
+    GPIO.setup(15, GPIO.OUT) #ACTIVACION DEL MOTOR DE LA CINTA
+    p = GPIO.PWM(15, 50) #ENVIO DE 50 PULSOS AL MOTOR
     p.start(5)
     
-    while diferencia<15 or texto!='parar cinta':
+    while diferencia<15 or texto!='parar cinta': #SE DETIENE CON LA ORDEN 'parar cinta' O CUANDO PASAN 15 SEGUNDOS SIN DETECTAR UN OBJETO
           
         
         i = GPIO.input(16) #Sensor
@@ -79,27 +81,27 @@ def iniciar_cinta(): #Funcion que pone en marcha la cinta
         diferencia = actual-inicial
         
         x = datetime.datetime.now()
-        x = x.strftime('%Y-%m-%d %H:%M:%S')
+        x = x.strftime('%Y-%m-%d %H:%M:%S') #FORMATEO DE LA FECHA DE Python PARA QUE CUADRE EN MySQL
         
-        if i == 0: #Si detecta objeto
+        if i == 0: #SI DETECTA OBJETO
             
-            numPieza+=1
+            numPieza+=1 #INCREMENTA EL NUMERO DE PIEZA (ID)
             
             inicial = int(round(time.time()*1))
             
-            GPIO.output(11, True) #Sonar buzzer
+            GPIO.output(11, True) #HACE SONAR EL BUZZER POR 0.1 SEGUNDOS
             time.sleep(0.1)
-            GPIO.output(11, False) #Apagar buzzer
+            GPIO.output(11, False) #APAGA EL BUZZER
                 
             time.sleep(0.1)
                 
-            if rc_time()>100:
+            if rc_time()>100: #LECTURA ANALOGICA DEL LDR (EL EMISOR LASER APUNTA CONSTANTEMENTE AL LDR)
                 print('Pieza grande')
                 tamano = "grande"
-                sql = "INSERT INTO objetos VALUES (%s, %s, %s)"
+                sql = "INSERT INTO objetos VALUES (%s, %s, %s)" 
                 val = [numPieza, tamano, x]
                 print(val)
-                mycursor.execute(sql, val)
+                mycursor.execute(sql, val) #INSERCION DE UN REGISTRO EN LA BASE DE DATOS
                 mydb.commit()
                 print(mycursor.rowcount, "fila(s) insertadas.")
         
@@ -109,18 +111,19 @@ def iniciar_cinta(): #Funcion que pone en marcha la cinta
                 sql = "INSERT INTO objetos VALUES (%s, %s, %s)"
                 val = [numPieza, tamano, x]
                 print(val)
-                mycursor.execute(sql, val)
+                mycursor.execute(sql, val) #INSERCION DE UN REGISTRO EN LA BASE DE DATOS
                 mydb.commit()
                 print(mycursor.rowcount, "fila(s) insertadas.")
       
     GPIO.setup(15, GPIO.IN) 
                 
-stop_listening = r.listen_in_background(m, callback, phrase_time_limit=3)
+stop_listening = r.listen_in_background(m, callback, phrase_time_limit=3) #EL PROGRAMA ESTA CONSTANTEMENTE ESCUCHANDO ORDENES DE VOZ, SEA CUAL SEA EL PUNTO EN EL QUE SE ENCUENTRE EL PROGRAMA
 
-while True: 
+while True: #EQUIVALENTE A LA FUNCION MAIN
+
     time.sleep(0.1)
-    if texto == 'iniciar cinta':
+    if texto == 'iniciar cinta': #SI SE INTRODUCE LA ORDEN 'iniciar cinta' LLAMA A LA FUNCION 'iniciar_cinta()'
        iniciar_cinta()
        texto = 0
-       GPIO.cleanup() #Limpiar GPIO
+       GPIO.cleanup() #LIMPIAR GPIO AL ACABAR
 
